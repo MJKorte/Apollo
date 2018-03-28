b32ffc9853331c37d23e7c0e8ed9f6bb0a5d2caa
RogierStegeman <rogier.stegeman@upcmail.nl>
2018-03-28 16:02:19 +0200

    Renaisaanance

--- src/main/java/com/mycompany/apollo/ORF.java
+++ src/main/java/com/mycompany/apollo/ORF.java
@@ -10,11 +10,14 @@ package com.mycompany.apollo;
  * @author rogie
  */
 public class ORF {
+
     public int orfID;
     public String sequentie;
     public int readNr;
-    
-    public ORF(int id, String seq, int read){
+    public boolean blasted;
+    public double E_Value;
+
+    public ORF(int id, String seq, int read) {
         orfID = id;
         sequentie = seq;
         readNr = read;
@@ -23,7 +26,21 @@ public class ORF {
     public int getSequenceLength() {
         return sequentie.length();
     }
-    
-    
-    
+
+    public void setBlastedTrue() {
+        this.blasted = true;
+    }
+
+    public boolean getBlasted() {
+        return this.blasted;
+    }
+
+    public void setE_Value(double e) {
+        this.E_Value = e;
+    }
+
+    public double getE_Value() {
+        return E_Value;
+    }
+
 }
