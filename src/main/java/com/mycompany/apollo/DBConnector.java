e59f49184e968d28638d264bd1c931dcac62fc42
Bram <bramgelsink@hotmail.com>
2018-03-28 15:48:32 +0200

    database leeg en werkend, go get em.

--- src/main/java/com/mycompany/apollo/DBConnector.java
+++ src/main/java/com/mycompany/apollo/DBConnector.java
@@ -25,8 +25,8 @@ public class DBConnector extends ApolloGUI {
         } catch (ClassNotFoundException e) {                                    //Exception handling voor vinden van de plugin
             e.printStackTrace();
         }
-        
-        
+//        System.out.println("hoi");
+
         Connection connect = null;                                              //Initialisatie v.d. connectie en statements
         PreparedStatement preStatement1 = null;
         PreparedStatement preStatement2 = null; 
@@ -35,7 +35,7 @@ public class DBConnector extends ApolloGUI {
             
 //        String myDriver = "com.mysql.jdbc.Driver";                              //Driver
               //Connection type : protocol : host : port : database
-        String myUrl = "jdbc:oracle:thin:@www.cytosine.nl:80:owe7_pg10";
+        String myUrl = "jdbc:oracle:thin:@cytosine.nl:1521:xe";
         String User = "owe7_pg10";
         String Password = "blaat1234";
         
@@ -45,13 +45,14 @@ public class DBConnector extends ApolloGUI {
             
             Statement statement;                                                //Initialisatie statement
             statement = connect.createStatement();
+            
         
-//            String getseqID_Query = "SELECT MAX(entry_ID) as entry_ID from entry"; //Query voor het ophalen van de grootste entry_ID
-//            ResultSet rs1 = statement.executeQuery(getseqID_Query);             //Uitvoeren van de query
-//            while (rs1.next()) {
-//                entryID = rs1.getInt("entry_ID");                                   //Waarde ophalen uit database
-//                }
-//            entryID +=1;                                                          //1 wordt opgeteld bij de oude sequentie voor een niewe entry id.
+            String getseqID_Query = "SELECT MAX(entry_ID) as entry_ID from entry"; //Query voor het ophalen van de grootste entry_ID
+            ResultSet rs1 = statement.executeQuery(getseqID_Query);             //Uitvoeren van de query
+            while (rs1.next()) {
+                entryID = rs1.getInt("entry_ID");                                   //Waarde ophalen uit database
+                }
+            entryID +=1;                                                          //1 wordt opgeteld bij de oude sequentie voor een niewe entry id.
             
             
             //query voor de ORF_ID
@@ -62,9 +63,23 @@ public class DBConnector extends ApolloGUI {
                 }      
             orfID +=1;                                                          //1 wordt opgeteld bij de oude sequentie van 
             
+            String getBlastID_Query = "SELECT MAX(blast_ID) as blast_ID from Blast_results";
+            ResultSet rs3 = statement.executeQuery(getBlastID_Query);
+            while (rs3.next()){
+                blastID = rs3.getInt("blast_ID");
+            }
+            blastID +=1;
             
+            String getBlastHitID_Query = "SELECT MAX(Blast_hit_ID) as Blast_hit_ID from Blast_hit";
+            ResultSet rs4 = statement.executeQuery(getBlastHitID_Query);
+            while (rs4.next()){
+                blast_hitID = rs4.getInt("Blast_hit_ID");
+            }
+            blast_hitID +=1;
+            
+            System.out.println("entry id "+entryID);
             //query voor het inserten in sequentie tabel
-            String insertSeqQuery = "INSERT INTO entry(entry_ID, sequentie, header/name) VALUES(?,?,?)"; //query 2
+            String insertSeqQuery = "INSERT INTO entry(entry_ID, sequence, entry_name) VALUES(?,?,?)"; //query 2
             preStatement1 = connect.prepareStatement(insertSeqQuery);           //PreStatement1 wordt gekoppeld aan de sequenties query
             preStatement1.setInt    (1, entryID);                            //Getal staat voor de locatie in de query, object staat voor variabele in javav die moet worden opgeslagen
             preStatement1.setString (2, dnaSeq);
@@ -73,30 +88,30 @@ public class DBConnector extends ApolloGUI {
             preStatement1.executeUpdate();                                      //update de tabel
             System.out.println("entry done");
             
-            
+            System.out.println("ORF id "+orfID);
             //query voor het inserten in ORF tabel
-            String insertORFQuery = "INSERT INTO ORF(ORF_ID, entry_entry_ID, orf_sequence, start_loc , stop_loc, read_frame) VALUES(?,?,?,?,?)"; //Query
+            String insertORFQuery = "INSERT INTO ORF(ORF_ID, entry_entry_ID, ORF_sequence, reading_frame) VALUES(?,?,?,?)"; //Query
             preStatement2 = connect.prepareStatement(insertORFQuery);              //PreStatement2 wordt gekoppeld aan de ORF query
             preStatement2.setInt     (1, orfID);
             preStatement2.setInt     (2, entryID);
             preStatement2.setString  (3, ORF_sequentie);
-            preStatement2.setInt     (4, start_positie);
-            preStatement2.setInt     (5, stop_positie);
-            preStatement2.setInt     (6, reading_frame);
+            preStatement2.setInt     (4, reading_frame);
             
             preStatement2.executeUpdate();                                      //update de tabel
             System.out.println("ORF done");
             
-            String insertBLASTQuery = "INSERT INTO BLAST_results(BLAST_ID, ORF_ORF_ID, hits)"; //query
+            System.out.println("blast id "+blastID);
+            String insertBLASTQuery = "INSERT INTO Blast_results(blast_ID, ORF_ORF_ID, hits) VALUES(?,?,?)"; //query
             preStatement3 = connect.prepareStatement(insertBLASTQuery);
             preStatement3.setInt    (1, blastID);
             preStatement3.setInt    (2, orfID);
-            preStatement3.setInt (3, hits);
+            preStatement3.setInt    (3, hits);
             
             preStatement3.executeUpdate();
             System.out.println("Blast result done");
             
-            String insertBLAST_hitQuery = "INSERT INTO BLAST_hit(BLAST_HIT_ID, E_Value, Identity, Accesion_code, Link)";
+            System.out.println("hit id"+blast_hitID);
+            String insertBLAST_hitQuery = "INSERT INTO Blast_hit(Blast_hit_ID, E_Value, identity_score, accession_code, Blast_results_blast_ID, url_link) VALUES(?,?,?,?,?,?)";
             preStatement4 = connect.prepareStatement(insertBLAST_hitQuery);
             preStatement4.setInt    (1, blast_hitID);
             preStatement4.setInt    (2, e_value);
