99d88e25db16c411f1a9de6deaeea4351a791f8f
Mark <MJ.deKorte@student.han.nl>
2018-03-29 17:19:27 +0200

    regel wat

--- src/main/java/com/mycompany/apollo/Blast.java
+++ src/main/java/com/mycompany/apollo/Blast.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.apollo;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,7 +31,12 @@ public class Blast extends Thread {
             
             //Blast stuff here
             ApolloGUI.ORFs.get(ORFid).setE_Value(0.000001);
-            //end blast stuff
+            try {
+                Process p = Runtime.getRuntime().exec("C:\\ProgramData\\Anaconda3\\python.exe yourapp.py");
+                //end blast stuff
+            } catch (IOException ex) {
+                Logger.getLogger(Blast.class.getName()).log(Level.SEVERE, null, ex);
+            }
             
             ApolloGUI.ORFs.get(ORFid).setBlastedTrue();
             ApolloGUI.blastButton.setEnabled(true);
